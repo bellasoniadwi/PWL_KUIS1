b74accda63b1875fdc606104aaec28ea15f391a9
bellasoniadwi <bellasoniadwi@gmail.com>
2022-03-14 18:57:02 +0700

    update isi beranda

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -11,9 +11,9 @@
          <div class="row d_flex">
             <div class=" col-xl-8 col-lg-8 col-md-8 col-12-9">
                <div class="text-bg">
-                  <h1>Blog<br> <span class="white1">Landing Page 2019</span></h1>
-                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,</p>
-                  <a href="#">Read More</a>
+                  <h1>Shine Bright<br> <span class="white1">Like A Diamond</span></h1>
+                  <p>The beauty of a woman must be seen from in her eyes, because that is the doorway to her heart, the place where love resides.</p>
+                  <a href="produk">Buy Now</a>
                </div>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 col-12-3">
