a4ed89213c8b7c48463f71b0041eca104fbe7fa6
bellasoniadwi <bellasoniadwi@gmail.com>
2022-03-14 21:04:05 +0700

    membuat controller barang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    //
+    public function index(){
+        $barangs = Barang::all();
+
+        return view('produk')
+            ->with('title', 'Produk')
+            ->with('barangs', $barangs);
+    }
 }
